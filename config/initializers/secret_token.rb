e4b14640374c4c260548c2adaf2858522a53c23e
Oscar Itera <menville.geo@yahoo.com>
2011-12-26 17:14:18 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Try5::Application.config.secret_token = '9257077d533747da08633fa849ed9c0e6235231dcfec8177dafc6fb69a1e18f158fb95b22ab5a28849e934ee26c5bff80ebfa8c61d6c6c0ceabd14afaf31f697'
